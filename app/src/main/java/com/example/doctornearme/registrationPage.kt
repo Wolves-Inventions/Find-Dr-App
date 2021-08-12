be8cc54fa15460ad83e4b66b844145608cf5e442
Ntokozo <Ntokozoflex99@gmail.com>
2021-08-12 15:05:45 +0200

    WOLVES-INVENTIONS

---
+++ app/src/main/java/com/example/doctornearme/registrationPage.kt
@@ -0,0 +1,11 @@
+package com.example.doctornearme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registrationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_page)
+    }
+}
